a6269a1b7e719e8220bd5a7b7e4019485534b4f1
Seb Ashton <seb@sebashton.com>
2016-04-04 12:58:27 +0100

    Pass tests

--- spec/unit/order_scopes_spec.rb
+++ spec/unit/order_scopes_spec.rb
@@ -2,21 +2,25 @@ describe Spree::Order do
   let!(:order) { create(:order_ready_to_ship) }
 
   context '#not_exported' do
-    subject { described_classe.not_exported }
+    subject { described_class.not_exported }
 
     it { is_expected.to_not be_empty }
 
     context 'when the order is exported' do
+      before { order.export }
+
       it { is_expected.to be_empty }
     end
   end
 
   context '#exported' do
-    subject { described_classe.exported }
+    subject { described_class.exported }
 
     it { is_expected.to be_empty }
 
     context 'when the order is exported' do
+      before { order.export }
+
       it { is_expected.to_not be_empty }
     end
   end
