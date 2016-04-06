fe59339fa94b4f04686a744615c612f828e429b1
Seb Ashton <seb@sebashton.com>
2016-04-06 09:46:58 +0100

    Remove bogus version option

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Spree::Core::Engine.routes.draw do
-  namespace :api, defaults: { format: 'json', versions: 1 } do
     resources :shipments do
       member do
         put :receive
         put :pick
       end
     end
+  namespace :api, defaults: { format: 'json' } do
 
     get '/orders/to_export', to: 'orders#to_export', as: 'orders_to_export'
 
