d5665f1fd1c520c5f4e82498bbd5c8bf05afd81f
Emile <emile.swarts123@gmail.com>
2016-09-28 15:35:13 +0100

    Mail order export instead of dumping it to the browser

--- app/controllers/concerns/solidus_wms/additional_orders_actions.rb
+++ app/controllers/concerns/solidus_wms/additional_orders_actions.rb
@@ -10,12 +10,11 @@ module SolidusWms
 
     def export_xlsx
       exporter = Spree::WmsConfig.order_xls_export_class.new
+      tmpfile = Tempfile.new('spree_orders.xlsx')
+      xlsx_file_contents(exporter).serialize(tmpfile.path)
+      Spree::WmsConfig.order_xls_export_mailer_class.latest(tmpfile.path).deliver_now
 
-      Mime::Type.register 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', :xlsx
-
-      send_data(xlsx_file_contents(exporter).to_stream.string,
-                type: Mime::XLSX,
-                disposition: 'attachment; filename=orders.xlsx')
+      render nothing: true
     end
 
     private
